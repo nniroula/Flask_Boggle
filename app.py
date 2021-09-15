4a99274c5532df0e1814b56fab23f4383b049fbf
Nabin <nkbhaie2017@gmail.com>
2021-09-15 11:02:00 -0600

    set up debugtoolbar to show up in a browser window

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -10,3 +10,8 @@ debug = DebugToolbarExtension(app)
 @app.route("/working")
 def test_flask_setup():
     return "Flask setup working properly"
+
+# to make debugtoolbar show up in the browser window, return some html file as debugtoolbar only appears after you render html file
+@app.route("/debugtoolbar")
+def show_debutoolbar_in_browser():
+    return render_template("debugtoolbar.html")
