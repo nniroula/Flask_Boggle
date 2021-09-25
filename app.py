a84dc23235c372d43595b941099c37b50091a00e
nabin <nkbhaie2017@gmail.com>
2021-09-25 15:01:59 -0600

    add more functionalities

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
+from flask.json import jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
@@ -27,6 +28,7 @@ def show_debutoolbar_in_browser():
 # make an instance of a class
 boggle = Boggle()
 
+"""
 @app.route("/")
 def display_boggle_board():
     make_board = boggle.make_board()
@@ -34,4 +36,42 @@ def display_boggle_board():
     # session['make_board'] = make_board  # this is the way
     return render_template("boggle_board.html", display_board = make_board) 
 
+"""
+
+@app.route("/")
+def display_boggle_board():
+    """ Display boggle board. """
+    make_board = boggle.make_board()
+    session['make_board'] = make_board
+    # session['make_board'] = make_board  # this is the way
+    return render_template("boggle_board.html", display_board = make_board) 
+
+
+# validate the guess word
+@app.route("/valid-guess-word")
+def validate_word():
+    """ Check if the word is valid."""
+    guess_word = request.args["guess_word"]
+    # put board in session
+    board = session['board']
+    # call a method for validating a word from Boggle class
+    res = boggle.check_valid_word(board, guess_word)
+    jsonify_word = jsonify({"output": res})
+    return jsonify_word
+
+# get score, update number of turns, update scores
+@app.route("/score", methods=["POST"])
+def show_score():
+    point = request.json["point"]
+    highest_point = session.get("highest_point", 0)
+    number_of_turn = session.get("number_of_turn", 0)
+
+    session['number_of_turn'] = number_of_turn + 1
+    session['highest_point'] = max(point, highest_point)
+    # convert it to the json and get back boolean value
+    result = point > highest_point
+    return jsonify(result)
+
+
+# axios.get("url")
 
